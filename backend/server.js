982a51ad88c28d677b32b19d92984dfde59cfb08
Badal Sharma <91386413+sharmabadal15@users.noreply.github.com>
2024-04-18 21:44:47 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const pool = require("./db");
 const app = express();
-const PORT = process.env.PORT || 2000;
+const PORT = process.env.PORT || 8000;
 
 app.use(express.json());
 app.use(cors());
